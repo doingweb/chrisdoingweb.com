54ccd8a8433bcbff17862a7ea9f00b9c4a50de0f
Chris Antes <nicenecktie@gmail.com>
2013-10-19 12:47:54 -0700

    Adding Modernizr. Finishing design. Setting up S3 deployment (still problems, at the moment).

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,13 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('build', function (environment) {
 		grunt.task.run('compass:' + (environment || 'production'));
+		grunt.task.run('modernizr');
 		grunt.task.run('jekyll:build');
 	});
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		credentials: grunt.file.readJSON('credentials.json'),
 		compass: {
 			options: {
 				sassDir: 'sass',
@@ -43,10 +45,33 @@ module.exports = function(grunt) {
 					livereload: true
 				}
 			}
+		},
+		modernizr: {
+			devFile: 'js/modernizr.js', // If we want a real dev version, we'll need to have two build stages (grunt and jekyll)
+			outputFile: 'js/modernizr.js'
+		},
+		s3: {
+			options: {
+				key: '<%= credentials.aws.key %>',
+				secret: '<%= credentials.aws.secret %>',
+				bucket: '<%= credentials.aws.bucket %>',
+				gzip: true,
+				access: 'public-read'
+			},
+			deploy: {
+				upload: [
+					{
+						src: '_site/**',
+						dest: ''
+					}
+				]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-jekyll');
+	grunt.loadNpmTasks('grunt-modernizr');
+	grunt.loadNpmTasks('grunt-s3');
 };
