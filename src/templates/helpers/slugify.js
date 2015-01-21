78646b139b381d698c9195a2884cb1c74685d447
Chris Antes <chris@chrisdoingweb.com>
2015-01-21 14:16:15 -0800

    Removing old Handlebars helper.

--- src/templates/helpers/slugify.js
+++
@@ -1,9 +0,0 @@
-module.exports.register = function (Handlebars, options, params) {
-  Handlebars.registerHelper('slugify', function(url) {
-    var matches = url.match(/(.*)index\.html$/);
-    if (matches) {
-      return matches[1];
-    }
-    return url;
-  });
-};
