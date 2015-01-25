c114bf586fb39dc5e070985007c0dc63270c3351
Chris Antes <chris@chrisdoingweb.com>
2015-01-25 01:28:04 -0800

    Refactoring gulp.src into pipeline function.

--- build/tasks/js.js
+++ build/tasks/js.js
@@ -8,22 +8,22 @@ var
 module.exports = {
   jquery: function jQueryJsTask (done) {
     return pipeline(
-      gulp.src('bower_components/jquery/dist/jquery.js'),
+      'bower_components/jquery/dist/jquery.js',
       'jquery.js');
   },
   foundation: function foundationJsTask (done) {
     return pipeline(
-      gulp.src([
+      [
         'bower_components/foundation/js/foundation/foundation.js',
         'bower_components/foundation/js/foundation/foundation.topbar.js',
         'src/js/init-foundation.js'
-      ]),
+      ],
       'foundation.js');
   }
 };
 
 function pipeline (source, output) {
-  return source
+  return gulp.src(source)
     .pipe(sourcemaps.init())
     .pipe(concat(output))
     .pipe(uglify())
