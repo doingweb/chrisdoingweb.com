6958006a5740ca66916ac6c6b52a913f4a48414b
Chris Antes <chris@chrisdoingweb.com>
2015-01-27 17:05:13 -0800

    Rev collecting for content.

--- build/tasks/content.js
+++ build/tasks/content.js
@@ -1,8 +1,8 @@
 var
+  _ = require('lodash'),
   gulp = require('gulp'),
   globs = require('../globs'),
   gulpFrontMatter = require('gulp-front-matter'),
-  _ = require('lodash'),
   gulpsmith = require('gulpsmith'),
   buildDate = require('metalsmith-build-date'),
   metadata = require('metalsmith-metadata'),
@@ -10,37 +10,50 @@ var
   meach = require('metalsmith-each'),
   markdown = require('metalsmith-markdown'),
   permalinks = require('metalsmith-permalinks'),
-  templates = require('metalsmith-templates');
+  templates = require('metalsmith-templates'),
+  merge = require('merge-stream'),
+  revCollector = require('gulp-rev-collector');
 
-module.exports = function contentTask () {
-  return gulp.src(globs.content)
-    .pipe(gulpFrontMatter()).on("data", function(file) {
-      _.assign(file, file.frontMatter);
-      delete file.frontMatter;
-    })
-    .pipe(gulpsmith()
-      .use(buildDate())
-      .use(metadata({
-        site: 'metadata/site.yaml',
-        contact: 'metadata/contact.yaml'
-      }))
-      .use(collections({
-        posts: 'blog/*.md'
-      }))
-      .use(meach(function (file, filename) {
-        if (file.collection.indexOf('posts') !== -1) {
-          file.template = 'post-layout.html';
-        }
-      }))
-      .use(markdown({
-        gfm: true
-      }))
-      .use(permalinks({
-        relative: false
-      }))
-      .use(templates({
-        engine: 'swig',
-        directory: 'src/templates'
-      })))
-    .pipe(gulp.dest('dist'));
+module.exports = function (options) {
+  options = _.assign({ rev: false }, options);
+
+  return function contentTask () {
+    var content = gulp.src(globs.content)
+      .pipe(gulpFrontMatter()).on("data", function(file) {
+        _.assign(file, file.frontMatter);
+        delete file.frontMatter;
+      })
+      .pipe(gulpsmith()
+        .use(buildDate())
+        .use(metadata({
+          site: 'metadata/site.yaml',
+          contact: 'metadata/contact.yaml'
+        }))
+        .use(collections({
+          posts: 'blog/*.md'
+        }))
+        .use(meach(function (file, filename) {
+          if (file.collection.indexOf('posts') !== -1) {
+            file.template = 'post-layout.html';
+          }
+        }))
+        .use(markdown({
+          gfm: true
+        }))
+        .use(permalinks({
+          relative: false
+        }))
+        .use(templates({
+          engine: 'swig',
+          directory: 'src/templates'
+        })));
+
+    if (options.rev) {
+      content = merge(content, gulp.src('build/.metadata/rev-manifest-*.json'))
+        .pipe(revCollector());
+    }
+
+    return content
+      .pipe(gulp.dest('dist'));
+  };
 };
--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ var
   js = require('./build/tasks/js'),
   serve = require('./build/tasks/serve');
 
-gulp.task('default', ['content', 'css', 'js']);
+gulp.task('default', ['build-dev']);
+
+gulp.task('build-dev', ['content', 'css', 'js']);
+gulp.task('build-deploy', ['content-deploy', 'css-deploy', 'js-deploy']);
 
 gulp.task('clean-all', clean.all);
 gulp.task('clean-content', clean.content);
@@ -14,12 +17,13 @@ gulp.task('clean-css', clean.css);
 gulp.task('clean-js', clean.js);
 gulp.task('clean-build-metadata', clean.buildMetadata);
 
-gulp.task('content', ['clean-content'], content);
+gulp.task('content', ['clean-content'], content());
+gulp.task('content-deploy', ['clean-content', 'css-deploy', 'js-deploy'], content({ rev: true }));
 
-gulp.task('css', ['clean-css'], css({ rev: false }));
+gulp.task('css', ['clean-css'], css());
 gulp.task('css-deploy', ['clean-css', 'clean-build-metadata'], css({ rev: true }));
 
-gulp.task('js', ['clean-js'], js({ rev: false }));
+gulp.task('js', ['clean-js'], js());
 gulp.task('js-deploy', ['clean-js', 'clean-build-metadata'], js({ rev: true }));
 
 gulp.task('serve', ['default'], serve);
