a73d515ddf0f033fa1d6634cd6aaaf222fb0d891
Chris Antes <chris@chrisdoingweb.com>
2015-01-28 21:07:48 -0800

    Adding deployment task. Working on 403 error from S3.

---
+++ build/tasks/deploy.js
@@ -0,0 +1,30 @@
+var
+  s3 = require('gulp-awspublish'),
+  fs = require('fs'),
+  credentials = JSON.parse(fs.readFileSync('credentials.json')),
+  gulp = require('gulp'),
+  s3options = require('gulp-awspublish-router'),
+  parallelize = require("concurrent-transform");
+
+module.exports = function deployTask () {
+  var publisher = s3.create(credentials.aws);
+
+  return gulp.src('dist/**/*')
+    .pipe(s3options({
+      routes: {
+        '^(css|js)[\\\\/]': {
+          cacheTime: 315360000,
+          gzip: true
+        },
+        '\\.(html|css|js|txt|eot|otf|svg|ttf)$': {
+          gzip: true
+        },
+        '^.+$': '$&'
+      }
+    }))
+    .pipe(publisher.publish())
+    // .pipe(parallelize(publisher.publish(), 32))
+    .pipe(publisher.sync())
+    // .pipe(publisher.cache())
+    .pipe(s3.reporter());
+};
--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,13 @@ var
   content = require('./build/tasks/content'),
   css = require('./build/tasks/css'),
   js = require('./build/tasks/js'),
-  serve = require('./build/tasks/serve');
+  serve = require('./build/tasks/serve'),
+  deploy = require('./build/tasks/deploy');
 
 gulp.task('default', ['build-dev']);
 
 gulp.task('build-dev', ['content', 'css', 'js']);
 gulp.task('build-deploy', ['content-deploy', 'css-deploy', 'js-deploy']);
-
-gulp.task('clean-all', ['clean-build-metadata'], clean.all);
-gulp.task('clean-content', clean.content);
-gulp.task('clean-css', clean.css);
-gulp.task('clean-js', clean.js);
-gulp.task('clean-build-metadata', clean.buildMetadata);
 
 gulp.task('content', ['clean-content'], content.dev);
 gulp.task('content-deploy', ['clean-content', 'css-deploy', 'js-deploy'], content.deploy);
@@ -26,4 +21,12 @@ gulp.task('css-deploy', ['clean-css', 'clean-build-metadata'], css.deploy);
 gulp.task('js', ['clean-js'], js.dev);
 gulp.task('js-deploy', ['clean-js', 'clean-build-metadata'], js.deploy);
 
+gulp.task('clean-all', ['clean-build-metadata'], clean.all);
+gulp.task('clean-content', clean.content);
+gulp.task('clean-css', clean.css);
+gulp.task('clean-js', clean.js);
+gulp.task('clean-build-metadata', clean.buildMetadata);
+
 gulp.task('serve', ['build-dev'], serve);
+// gulp.task('deploy', ['build-deploy'], deploy);
+gulp.task('deploy', deploy);
