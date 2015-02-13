5308d06857cfc2a5cc9430939c0bc93d66ea69d5
Chris Antes <chris@chrisdoingweb.com>
2015-02-13 02:10:08 -0800

    Adding note for the CSS @import workaround.

--- build/scripts/bower-sassify.js
+++ build/scripts/bower-sassify.js
@@ -3,6 +3,7 @@ var
   path = require('path'),
   glob = require('glob');
 
+// This is a workaround for not being able to `@import` regular CSS files. See https://github.com/sass/sass/issues/556.
 console.log('Renaming highlight.js styles for Sass...');
 glob(path.join('bower_components', 'highlightjs', 'styles', '*.css'), function (err, files) {
   var renamed = files.map(function (file) {
